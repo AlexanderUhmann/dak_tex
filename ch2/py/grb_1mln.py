468182676c97c6e47741318808249fdaad7a1d61
Assem200409 <assemk2004@gmail.com>
2026-03-12 21:50:59 +0500

    Add files via upload

--- ch2/py/grb_1mln.py
+++ ch2/py/grb_1mln.py
@@ -3,7 +3,6 @@ from sklearn.ensemble import GradientBoostingClassifier
 X_train, X_test, Y_train, Y_test = train_test_split(X_pay, y_, test_size=0.25, random_state=0)
 boost_gr = GradientBoostingClassifier()
 boost_gr.fit(X_train, Y_train)
-boost_gr.score(X_train, Y_train), boost_gr.score(X_test, Y_test)
 
 y_pred_train = boost_gr.predict(X_train)
 y_proba_train = boost_gr.predict_proba(X_train)
--- ch2/py/rfc_1mln.py
+++ ch2/py/rfc_1mln.py
@@ -3,7 +3,6 @@ from sklearn.ensemble import RandomForestClassifier
 X_train, X_test, Y_train, Y_test = train_test_split(X_pay, y_, test_size=0.25, random_state=0)
 rfc = RandomForestClassifier()
 rfc.fit(X_train, Y_train)
-rfc.score(X_train, Y_train), rfc.score(X_test, Y_test)
 
 y_pred_train = rfc.predict(X_train)
 y_proba_train = rfc.predict_proba(X_train)
